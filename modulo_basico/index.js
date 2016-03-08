c8a112558fbf22ed52d7129e4c59329f84dff3f5
Jesus Velasco <jesus.velasco@babel.es>
2016-03-08 11:57:50 +0100

    Vemos como utilizar modulos

---
+++ modulo_basico/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var modulo = require ("./modulo");
+
+console.log(modulo);
+//modulo.vuela();
+
+modulo.objeto.vuela();
---
+++ modulo_basico/modulo.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var objeto = {
+    vuela: function(){
+        console.log("volando voy");
+    }
+};
+
+// module.exports = objeto;
+
+exports.objeto = objeto;
