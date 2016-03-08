f49fd6eb5479a27eb0bacc9c2e07a4f0e0d2d23e
Jesus Velasco <jesus.velasco@babel.es>
2016-03-08 10:08:47 +0100

    Clousure basico creando objetos que tengan metodos en el return

---
+++ clousure.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var creaAgente = function(_edad) {
+    var edad = _edad;
+    return {
+        dimeEdad: function(){
+            console.log("Yo tengo " + edad);
+            return edad;
+        }
+        // ponEdad: function(){
+            
+        // }
+    }
+};
+
+//Cuando le llame la variable agente tendra un objeto
+var agente = creaAgente(30);
+
+var smith = creaAgente(22);
+
+console.log(agente.dimeEdad(), smith.dimeEdad());
+
+setTimeout(agente.dimeEdad, 1000);
+setTimeout(smith.dimeEdad, 1000);
+
