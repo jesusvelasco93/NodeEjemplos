3129cda58031411bd481dc86437dac7b45e5f22e
Jesus Velasco <jesus.velasco@babel.es>
2016-03-09 12:23:50 +0100

    Comentarios para clarificar el codigo del ultimo comit

--- app_express/prueba/models/user-model.js
+++ app_express/prueba/models/user-model.js
@@ -1,14 +1,17 @@
 "use strict";
 
+// Datos
 var users = [
     { name: "Smith", age: "30" },
     { name: "Juan", age: "20" }
 ];
 
+// Metodos del modelo
 var user = {
     getUsers: function(){
         return users;
     }
 };
 
+// Exportamos el modelo
 module.exports = user;
